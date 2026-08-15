17304d4b6f4614b14292e7856726713083b18630
Bena <no-reply@example.com>
2026-08-15 10:49:35 +0200

    make lint happy

--- src/types/react-media.d.ts
+++ src/types/react-media.d.ts
@@ -3,6 +3,8 @@ import 'react'
 declare module 'react' {
   // HTML media elements support referrerpolicy; React's VideoHTMLAttributes omit it.
   // https://html.spec.whatwg.org/multipage/media.html#attr-media-referrerpolicy
+  // Type param must stay `T` to merge with React's MediaHTMLAttributes<T>.
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars -- required by declaration merge
   interface MediaHTMLAttributes<T> {
     referrerPolicy?: HTMLAttributeReferrerPolicy | undefined
   }
